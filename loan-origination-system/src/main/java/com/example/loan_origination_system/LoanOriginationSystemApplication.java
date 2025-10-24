13d6046fd3bb881fe12314452afe2cfff5a78278
Akshat Pahwa <apahwa@plansource.com>
2025-10-24 10:44:40 +0530

    Skeleton-structure

---
+++ loan-origination-system/src/main/java/com/example/loan_origination_system/LoanOriginationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.loan_origination_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanOriginationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanOriginationSystemApplication.class, args);
+	}
+
+}
